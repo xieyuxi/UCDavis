7046c3c0d40c059020a65ce27a7566a28507219e
tiwanaca <briandonatiello@gmail.com>
2015-06-28 16:56:32 +0100

    Project Set-Up

---
+++ UCDavisCloud/Platform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Platform.Startup))]
+namespace Platform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
